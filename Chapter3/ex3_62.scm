04ff93527fd730dc3ba6894d7860828761188c7c
Vinu Rajashekhar <vinutheraj@gmail.com>
2018-04-11 00:16:31 -0700

    Tangent series.

--- Chapter3/ex3_62.scm
+++ Chapter3/ex3_62.scm
@@ -6,7 +6,10 @@
   (if (= (stream-car S) 0)
       (error "Cannot invert series! Stream starting with 0 constant term.")
       (scale-stream (invert-unit-series (scale-stream S (/ 1 (stream-car S))))
-                    (/ 1 stream-car S))))
+                    (/ 1 (stream-car S)))))
 
 (define (div-series S1 S2)
   (mul-series S1 (invert-series S2)))
+
+(define tangent-series
+  (div-series sine-series cosine-series))
